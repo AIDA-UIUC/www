b9de5bf8a64437906f71c484f391cd8948e82c3e
Shaw Kagawa <skagawa2@illinois.edu>
2019-10-13 14:50:38 -0500

    changed email address to new address

--- config.js
+++ config.js
@@ -1,31 +1,31 @@
 module.exports = {
-  siteTitle: 'Data Science @ Illinois', // <title>
-  manifestName: 'adsa',
-  manifestShortName: 'Landing', // max 12 characters
-  manifestStartUrl: '/',
-  manifestBackgroundColor: '#663399',
-  manifestThemeColor: '#663399',
-  manifestDisplay: 'standalone',
-  manifestIcon: './src/assets/images/adsa-logo-notext.png',
-  heading: 'The Association of Data Science and Analytics',
-  subHeading:
-    'We are the premier data science student organization at the University of Illinois',
+  siteTitle : 'Data Science @ Illinois', // <title>
+  manifestName : 'adsa',
+  manifestShortName : 'Landing', // max 12 characters
+  manifestStartUrl : '/',
+  manifestBackgroundColor : '#663399',
+  manifestThemeColor : '#663399',
+  manifestDisplay : 'standalone',
+  manifestIcon : './src/assets/images/adsa-logo-notext.png',
+  heading : 'The Association of Data Science and Analytics',
+  subHeading :
+      'We are the premier data science student organization at the University of Illinois',
   // social
-  socialLinks: [
+  socialLinks : [
     {
-      icon: 'fa-github',
-      name: 'Github',
-      url: 'https://github.com/adsa-uiuc',
+      icon : 'fa-github',
+      name : 'Github',
+      url : 'https://github.com/adsa-uiuc',
     },
     {
-      icon: 'fa-facebook',
-      name: 'Facebook',
-      url: 'https://facebook.com/adsauiuc',
+      icon : 'fa-facebook',
+      name : 'Facebook',
+      url : 'https://facebook.com/adsauiuc',
     },
     {
-      icon: 'fa-envelope-o',
-      name: 'Email',
-      url: 'mailto:general.adsa@gmail.com',
+      icon : 'fa-envelope-o',
+      name : 'Email',
+      url : 'mailto:uiuc.adsa@gmail.com',
     },
   ],
 };
